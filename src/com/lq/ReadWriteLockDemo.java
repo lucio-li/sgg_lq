4b2adc6be2c4d86cec3eb4a63a2f3f05bbf51905
liqiao <592782252@qq.com>
2020-01-19 17:29:49 +0800

    初始化

--- src/com/lq/ReadWriteLockDemo.java
+++ src/com/lq/ReadWriteLockDemo.java
@@ -1,8 +1,25 @@
 package com.lq;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author liqiao
  * 2020/1/19
  */
 public class ReadWriteLockDemo {
+    private volatile Map<String, Object> map = new HashMap<>();
+
+    public void put(String key, Object value) {
+        System.out.println(Thread.currentThread().getName() + "\t 正在写入：" + key);
+        try {
+            TimeUnit.MICROSECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        map.put(key, value);
+        System.out.println(Thread.currentThread().getName() + "\t 写入完成");
+
+    }
 }
